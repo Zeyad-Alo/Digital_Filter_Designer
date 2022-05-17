c00f4481c1392b4905424547d0ecf82f956e5ddb
Zeyad-Alo <zeyadmansour2@gmail.com>
2022-05-17 18:16:10 +0200

    7aga

--- home.py
+++ home.py
@@ -1,14 +1,80 @@
+from dash import dcc
 from dash import html
 from apps.navbar import create_navbar
+import dash_bootstrap_components as dbc
+import plotly.express as px
+from app import app
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
 
-nav = create_navbar()
 
-header = html.H3('Welcome to home page!')
+zplane_card = dbc.Card(
+    [
+        dbc.CardHeader("Z-Plane"),
+        dbc.CardBody(dcc.Graph(id='z_plane')),
+    ]
+    )
+
+options_card = dbc.Card(
+    [
+        dbc.CardHeader("Preferences"),
+        dbc.CardBody("help"),
+    ],
+    #className="mt-4",
+)
+
+mag_card = dbc.Card(
+    [
+        dbc.CardHeader("Magnitude Response"),
+        dbc.CardBody(dcc.Graph(id='mag_response')),
+    ],
+    #className="mt-4",
+)
+
+phase_card = dbc.Card(
+    [
+        dbc.CardHeader("Phase Response"),
+        dbc.CardBody(dcc.Graph(id='phase_response')),
+    ],
+    #className="mt-4",
+)
+
+
+def design_tab_layout():
+    layout = html.Div([
+        dbc.Row([
+            dbc.Col([zplane_card, html.Br(), options_card], lg=4),
+            dbc.Col([mag_card, html.Br(), phase_card])
+            ])
+        ],            
+                      )
+    return layout
 
 
 def create_page_home():
     layout = html.Div([
-        nav,
-        header,
-    ])
+        dbc.Tabs(
+            [
+                dbc.Tab(label="Design!", tab_id="design_filter"),
+                dbc.Tab(label="Implement!", tab_id="apply_implement"),
+            ],
+            id="tabs",
+            active_tab="design_filter",
+        ),
+        html.Div(id="tab-content", className="p-4"),
+        ],
+          className="mx-4"          
+                      )
     return layout
+
+
+@app.callback(
+    Output("tab-content", "children"),
+    Input("tabs", "active_tab"),
+)
+def render_tab_content(active_tab):
+    if active_tab == "design_filter":
+        return design_tab_layout()
+        print("design")
+    else:
+        pass
--- index.py
+++ index.py
@@ -1,5 +1,6 @@
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 from home import create_page_home
 from apps.page_2 import create_page_2
@@ -9,22 +10,19 @@ from app import app
 server = app.server
 app.config.suppress_callback_exceptions = True
 
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
-
-
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/page-2':
-        return create_page_2()
-    if pathname == '/page-3':
-        return create_page_3()
-    else:
-        return create_page_home()
+app.layout = dbc.Container(
+    [
+        html.H5(
+                    "Digital Filter Designer",
+                    className="text-center bg-dark text-white p-2",
+                ),
+        create_page_home(),
+    ],
+    className="p-0",
+    
+    fluid=True,
+                      )
 
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
