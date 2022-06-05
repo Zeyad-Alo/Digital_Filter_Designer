60f0904ca8f2f630191ee96d79689bb21ab779fc
Mohamed N. Gaafar <46086535+mo-gaafar@users.noreply.github.com>
2022-06-05 08:55:09 +0200

    cache clearing

--- app.py
+++ app.py
@@ -10,6 +10,7 @@
 
 import dash
 import dash_bootstrap_components as dbc
+from flask_caching import Cache
 
 # Toggle the themes at [dbc.themes.LUX]
 # The full list of available themes is:
@@ -19,4 +20,9 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.QUARTZ, dbc.icons.BOOTSTRAP])
+cache = Cache(app.server, config={
+    'CACHE_TYPE': 'simple',
+    'CACHE_DIR': 'cache-directory'
+})
+cache.clear()
 server = app.server
